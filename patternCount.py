f2c73cd8a90e136a6afc2df99206d368ffa2a005
b0bb1e <52177356+b0bb1e@users.noreply.github.com>
2019-07-29 16:21:42 -0700

    Create patternCount.py

---
+++ patternCount.py
@@ -0,0 +1,6 @@
+def patternCount(text, pattern):
+    count = 0
+    for i in range(len(text) - len(pattern) + 1):
+        if text[i:i + len(pattern)] == pattern:
+            count = count + 1
+    return count

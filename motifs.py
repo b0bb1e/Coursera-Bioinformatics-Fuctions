f38815d79074a9a99d8fca05a0a279faf7cbaa0c
b0bb1e <52177356+b0bb1e@users.noreply.github.com>
2019-08-02 09:43:47 -0700

    Create motifs.py

---
+++ motifs.py
@@ -0,0 +1,21 @@
+def motifs(profile, dna):
+    bestMotifs = []
+    for string in dna:
+        bestMotifs.append(profileMostProbableKmer(string, len(profile["A"]), profile))
+    return bestMotifs
+    
+def pr(text, profile):
+    p = 1
+    for i in range(len(text)):
+        p *= profile[text[i]][i]
+    return p
+    
+def profileMostProbableKmer(text, k, profile):
+    mostP = -1
+    mostK = ""
+    for i in range(len(text) - k + 1):
+        newP = pr(text[i:i + k], profile)
+        if newP > mostP:
+            mostP = newP
+            mostK = text[i:i + k]
+    return mostK
